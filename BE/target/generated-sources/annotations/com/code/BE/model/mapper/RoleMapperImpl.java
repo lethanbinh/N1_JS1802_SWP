fca1307c5efa3d5c9dec324ab92fe95ec95519d5
Binh <binhltse173315@fpt.edu.vn>
2024-05-23 11:45:53 +0700

    Binh - Update Validation rules and messages

---
+++ BE/target/generated-sources/annotations/com/code/BE/model/mapper/RoleMapperImpl.java
@@ -0,0 +1,68 @@
+package com.code.BE.model.mapper;
+
+import com.code.BE.model.dto.request.RoleRequest;
+import com.code.BE.model.dto.response.RoleResponse;
+import com.code.BE.model.entity.Role;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-22T19:43:11+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
+)
+@Component
+public class RoleMapperImpl implements RoleMapper {
+
+    @Override
+    public RoleResponse toResponse(Role user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        RoleResponse roleResponse = new RoleResponse();
+
+        return roleResponse;
+    }
+
+    @Override
+    public List<RoleResponse> toResponseList(List<Role> roleList) {
+        if ( roleList == null ) {
+            return null;
+        }
+
+        List<RoleResponse> list = new ArrayList<RoleResponse>( roleList.size() );
+        for ( Role role : roleList ) {
+            list.add( toResponse( role ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public Role toEntity(RoleRequest roleRequest) {
+        if ( roleRequest == null ) {
+            return null;
+        }
+
+        Role role = new Role();
+
+        return role;
+    }
+
+    @Override
+    public List<Role> toEntityList(List<RoleRequest> roleRequestList) {
+        if ( roleRequestList == null ) {
+            return null;
+        }
+
+        List<Role> list = new ArrayList<Role>( roleRequestList.size() );
+        for ( RoleRequest roleRequest : roleRequestList ) {
+            list.add( toEntity( roleRequest ) );
+        }
+
+        return list;
+    }
+}
---
+++ BE/target/generated-sources/annotations/com/code/BE/model/mapper/UserMapperImpl.java
@@ -0,0 +1,122 @@
+package com.code.BE.model.mapper;
+
+import com.code.BE.model.dto.request.UserRequest;
+import com.code.BE.model.dto.response.UserResponse;
+import com.code.BE.model.entity.Role;
+import com.code.BE.model.entity.Stall;
+import com.code.BE.model.entity.User;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-05-23T07:44:17+0700",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserResponse toResponse(User user) {
+        if ( user == null ) {
+            return null;
+        }
+
+        UserResponse userResponse = new UserResponse();
+
+        Integer id = userRoleId( user );
+        if ( id != null ) {
+            userResponse.setRoleId( String.valueOf( id ) );
+        }
+        Integer id1 = userStallId( user );
+        if ( id1 != null ) {
+            userResponse.setStallId( String.valueOf( id1 ) );
+        }
+        userResponse.setId( user.getId() );
+        userResponse.setUsername( user.getUsername() );
+        userResponse.setRegisterDate( user.getRegisterDate() );
+        userResponse.setPhone( user.getPhone() );
+        userResponse.setEmail( user.getEmail() );
+        userResponse.setAddress( user.getAddress() );
+        userResponse.setAvatar( user.getAvatar() );
+        userResponse.setPointBonus( user.getPointBonus() );
+        userResponse.setBirthday( user.getBirthday() );
+        userResponse.setStatus( user.isStatus() );
+
+        return userResponse;
+    }
+
+    @Override
+    public List<UserResponse> toResponseList(List<User> userList) {
+        if ( userList == null ) {
+            return null;
+        }
+
+        List<UserResponse> list = new ArrayList<UserResponse>( userList.size() );
+        for ( User user : userList ) {
+            list.add( toResponse( user ) );
+        }
+
+        return list;
+    }
+
+    @Override
+    public User toEntity(UserRequest userRequest) {
+        if ( userRequest == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setUsername( userRequest.getUsername() );
+        user.setPassword( userRequest.getPassword() );
+        user.setPhone( userRequest.getPhone() );
+        user.setEmail( userRequest.getEmail() );
+        user.setAddress( userRequest.getAddress() );
+        user.setAvatar( userRequest.getAvatar() );
+        user.setBirthday( userRequest.getBirthday() );
+        user.setStatus( userRequest.isStatus() );
+
+        return user;
+    }
+
+    @Override
+    public List<User> toEntityList(List<UserRequest> userRequestList) {
+        if ( userRequestList == null ) {
+            return null;
+        }
+
+        List<User> list = new ArrayList<User>( userRequestList.size() );
+        for ( UserRequest userRequest : userRequestList ) {
+            list.add( toEntity( userRequest ) );
+        }
+
+        return list;
+    }
+
+    private Integer userRoleId(User user) {
+        if ( user == null ) {
+            return null;
+        }
+        Role role = user.getRole();
+        if ( role == null ) {
+            return null;
+        }
+        int id = role.getId();
+        return id;
+    }
+
+    private Integer userStallId(User user) {
+        if ( user == null ) {
+            return null;
+        }
+        Stall stall = user.getStall();
+        if ( stall == null ) {
+            return null;
+        }
+        int id = stall.getId();
+        return id;
+    }
+}
