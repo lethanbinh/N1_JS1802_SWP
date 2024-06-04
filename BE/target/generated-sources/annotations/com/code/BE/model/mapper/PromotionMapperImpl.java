67ea7a17e1a0d0f0a284b113a5bfae2128e188f6
anle4coding <lethuan.esther@gmail.com>
2024-06-05 00:07:13 +0700

    An - Update new src, Sidebar, Header, Navbar, Logout, Home UI

--- BE/target/generated-sources/annotations/com/code/BE/model/mapper/PromotionMapperImpl.java
+++ BE/target/generated-sources/annotations/com/code/BE/model/mapper/PromotionMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-06-03T19:55:04+0700",
+    date = "2024-06-04T23:53:01+0700",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 22.0.1 (Oracle Corporation)"
 )
 @Component
