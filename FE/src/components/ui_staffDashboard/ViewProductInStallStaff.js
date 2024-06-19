81a4a7f98d07e9c70dbf9e2bc4c11915dda23fe2
imeffort <anltse173297@fpt.edu.vn>
2024-06-20 02:45:49 +0700

    ThienAn - link api Product in stall

--- FE/src/components/ui_staffDashboard/ViewProductInStallStaff.js
+++ FE/src/components/ui_staffDashboard/ViewProductInStallStaff.js
@@ -1,181 +1,121 @@
 import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCol,
-  CDropdownHeader,
-  CFormInput,
-  CFormTextarea,
-  CRow,
-  CTable,
-  CTableBody,
-  CTableDataCell,
-  CTableHead,
-  CTableHeaderCell,
-  CTableRow
-} from '@coreui/react'
-import React, { useState } from 'react'
-
-const StallProduct = () => {
-    const [data, setData] = useState([
-        { id: 1, code: 'ABC', description: 'J001', name: 'Elegant gold necklace', quantity: 100, price: 100.00, type: 'necklace' },
-        { id: 2, code: 'XYZ', description: 'J001', name: 'Elegant gold necklace', quantity: 50, price: 100.00, type: 'necklace' },
-        { id: 3, code: 'DEF', description: 'J001', name: 'Elegant gold necklace', quantity: 20, price: 100.00, type: 'necklace' },
-    ])
-
-    const [editingRow, setEditingRow] = useState(null)
-    const [formData, setFormData] = useState({})
-
-    const handleEdit = (id) => {
-        setEditingRow(id)
-        setFormData(data.find((row) => row.id === id))
+    CButton,
+    CCard,
+    CCardBody,
+    CCardHeader,
+    CCol,
+    CDropdownHeader,
+    CFormInput,
+    CFormTextarea,
+    CRow,
+    CTable,
+    CTableBody,
+    CTableDataCell,
+    CTableHead,
+    CTableHeaderCell,
+    CTableRow
+  } from '@coreui/react'
+  import React, { useState, useEffect } from 'react'
+  import fetchData from '../../util/ApiConnection';
+  import UserStorage from '../../util/UserStorage';
+  
+  
+  const StallProduct = () => {
+    const [userInfo, setUserInfo] = useState(UserStorage.getAuthenticatedUser())
+    const [stallName, setStallName] = useState('')
+    const [data, setData] = useState([])
+    const [error, setError] = useState(null)
+    const [show, setShow] = useState(false)
+  
+    const loadData = async (stallName) => {
+      try {
+        const productData = await fetchData(`http://localhost:8080/api/v1/products/stallName/${stallName}`, 'GET', null, userInfo.accessToken)
+        setData(productData.payload)
+  
+        setError(null)
+      } catch (error) {
+        setError(error.message)
+      }
     }
-
-    const handleInputChange = (event) => {
-        setFormData({ ...formData, [event.target.name]: event.target.value })
-    }
-
-    const handleSave = () => {
-        const newData = data.map((row) => {
-            if (row.id === editingRow) {
-                return { ...row, ...formData }
-            }
-            return row
-        })
-        setData(newData)
-        setEditingRow(null)
-    }
-    const handleAddNew = () => {
-        const newRow = {
-            id: data.length + 1,
-            code: '',
-            name: '',
-            description: '',
-            status: '',
-            type: '',
-        }
-        setData([...data, newRow])
-        setEditingRow(newRow.id)
-    }
-
+  
+    useEffect(() => {
+      loadData(stallName)
+    }, [stallName])
+  
+  
     return (
-        <CRow>
-            <CCol xs={12}>
-                <CCard className="mb-4">
-                    <CCardHeader>
-                        <CDropdownHeader><strong>Stall...</strong></CDropdownHeader>
-                    </CCardHeader>
-                    <CCardBody>
-                    <div style={{ height: '500px', overflow: 'auto' }}>
-                        <CTable>
-                            <CTableHead>
-                                <CTableRow>
-                                    <CTableHeaderCell scope="col">Id</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Code</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Description</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Name</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Quantity</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Price</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Type</CTableHeaderCell>
-                                    <CTableHeaderCell scope="col">Action</CTableHeaderCell>
-                                </CTableRow>
-                            </CTableHead>
-                            <CTableBody>
-                                {data.map((row) => (
-                                    <CTableRow key={row.id}>
-                                        <CTableHeaderCell scope="row">{row.id}</CTableHeaderCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CFormInput
-                                                    type="text"
-                                                    name="code"
-                                                    value={formData.code}
-                                                    onChange={handleInputChange}
-                                                />
-                                            ) : (
-                                                row.code
-                                            )}
-                                        </CTableDataCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CFormInput
-                                                    type="text"
-                                                    name="description"
-                                                    value={formData.description}
-                                                    onChange={handleInputChange}
-                                                />
-                                            ) : (
-                                                row.description
-                                            )}
-                                        </CTableDataCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CFormTextarea
-                                                    name="description"
-                                                    value={formData.name}
-                                                    onChange={handleInputChange}
-                                                />
-                                            ) : (
-                                                row.description
-                                            )}
-                                        </CTableDataCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CFormInput
-                                                    type="text"
-                                                    name="quantity"
-                                                    value={formData.quantity}
-                                                    onChange={handleInputChange}
-                                                />
-                                            ) : (
-                                                row.quantity
-                                            )}
-                                        </CTableDataCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CFormInput
-                                                    type="text"
-                                                    name="price"
-                                                    value={formData.price}
-                                                    onChange={handleInputChange}
-                                                />
-                                            ) : (
-                                                row.price
-                                            )}
-                                        </CTableDataCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CFormInput
-                                                    type="text"
-                                                    name="type"
-                                                    value={formData.type}
-                                                    onChange={handleInputChange}
-                                                />
-                                            ) : (
-                                                row.type
-                                            )}
-                                        </CTableDataCell>
-                                        <CTableDataCell>
-                                            {editingRow === row.id ? (
-                                                <CButton color="success" onClick={handleSave}>
-                                                    Save
-                                                </CButton>
-                                            ) : (
-                                                <CButton color="info" onClick={() => handleEdit(row.id)}>
-                                                    Edit
-                                                </CButton>
-                                            )}
-                                        </CTableDataCell>
-                                    </CTableRow>
-                                ))}
-                            </CTableBody>
-                        </CTable>
-                    </div>
-                    </CCardBody>
-                </CCard>
-            </CCol>
-        </CRow>
+      <CRow>
+        <CCol xs={12}>
+          <CCard className="mb-4">
+            <CCardHeader>
+              <CDropdownHeader>
+                <strong>
+                  <CFormInput
+                    type="text"
+                    placeholder='Stall Name'
+                    name="stallName"
+                    value={stallName}
+                    onChange={(event) => setStallName(event.target.value)}
+                  />
+                </strong>
+              </CDropdownHeader>
+  
+              <CButton onClick={() => setShow(!show)} type='submit' color="info">
+                Search
+              </CButton>
+            </CCardHeader>
+            {show &&
+              <CCardBody>
+                <div style={{ height: '500px', overflow: 'auto' }}>
+                  <CTable>
+                    <CTableHead>
+                      <CTableRow>
+                        <CTableHeaderCell scope="col">Id</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Code</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Quantity</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Purchase Price</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Sell Price</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Type</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Weight</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Size</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Stall Location</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Bar Code Text</CTableHeaderCell>
+                        <CTableHeaderCell scope="col">Image</CTableHeaderCell>
+                      </CTableRow>
+                    </CTableHead>
+                    <CTableBody>
+                      {
+                        data.map((row, index) => (
+                          <CTableRow key={row.id}>
+                            <CTableHeaderCell scope="row">{row.id}</CTableHeaderCell>
+                            <CTableDataCell>{row.code}</CTableDataCell>
+                            <CTableDataCell>{row.description}</CTableDataCell>
+                            <CTableDataCell>{row.name}</CTableDataCell>
+                            <CTableDataCell>{row.quantity}</CTableDataCell>
+                            <CTableDataCell>{row.purchasePrice}</CTableDataCell>
+                            <CTableDataCell>{row.sellPrice}</CTableDataCell>
+                            <CTableDataCell>{row.type}</CTableDataCell>
+                            <CTableDataCell>{row.weight}</CTableDataCell>
+                            <CTableDataCell>{row.size}</CTableDataCell>
+                            <CTableDataCell>{row.stallLocation}</CTableDataCell>
+                            <CTableDataCell>{row.barCodeText}</CTableDataCell>
+                            <CTableDataCell>
+                              <img src={row.image} alt={row.name} style={{ width: '50px', height: '50px' }} /> {/* Display image */}
+                            </CTableDataCell>
+                          </CTableRow>
+                        ))
+                      }
+                    </CTableBody>
+                  </CTable>
+                </div>
+              </CCardBody>
+            }
+          </CCard>
+        </CCol>
+      </CRow>
     )
-}
-export default StallProduct
+  }
+  export default StallProduct
+  
