3feb034163e512baeb77d1a77a690ee7b66e5f7e
Binh <binhltse173315@fpt.edu.vn>
2024-06-24 09:05:14 +0700

    FE - fix bugs

--- FE/src/components/AppFooter.js
+++ FE/src/components/AppFooter.js
@@ -4,7 +4,6 @@ import { CFooter } from '@coreui/react'
 const AppFooter = () => {
   return (
     <CFooter className="text-center py-2">
-      <div className="ms-1">JEWELRY Store &copy; 2024 creativeLabs.</div>
     </CFooter>
   )
 }
